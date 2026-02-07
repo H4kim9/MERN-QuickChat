01c2cc065d530a685cbf80ca8f5ec1fb192c98a5
Hakim <abdelhakim111mime@gmail.com>
2026-02-08 00:26:46 +0100

    server.js correct for vercel

--- server/server.js
+++ server/server.js
@@ -49,9 +49,9 @@ app.use('/api/message', messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV !== "production"){  
+    const PORT = process.env.PORT || 5000;
     server.listen(PORT, ()=>{
         console.log(`Server is running on PORT: http://localhost:${PORT}`)
     })
